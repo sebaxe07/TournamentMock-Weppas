a9ef6628fcc1d5dbf0bf80f42b87033a2d0f78c8
CodeKataBattle SE2 <158226386+codekatabattle-se2@users.noreply.github.com>
2024-02-10 15:21:18 +0100

    Initial commit Add/update file src/main/java/com/example/StringUtils.java

---
+++ src/main/java/com/example/StringUtils.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class StringUtils {
+    public static String reverseString(String str) {
+        return null;
+    }
+
+    public static String capitalizeFirstLetter(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return null;
+    }
+}
